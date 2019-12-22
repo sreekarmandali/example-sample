d4ecad068eec5c2009f7b8719dfbec7d63c97ceb
sreekarmandali <sreekarmandali1@gmail.com>
2019-12-23 01:24:16 +0530

    Deleting old code

--- tests/unit/example.spec.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-   
-  });
-});
